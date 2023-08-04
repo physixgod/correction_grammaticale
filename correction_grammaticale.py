9ebec168bae545fda02a39fda03c21d6ed559318
physixgod <116004249+physixgod@users.noreply.github.com>
2023-08-04 16:03:24 +0100

    Update correction_grammaticale.py

--- correction_grammaticale.py
+++ correction_grammaticale.py
@@ -1,7 +1,10 @@
-from gingerit.gingerit import GingerIt
-def correct(text):
-    new_text = GingerIt().parse(text)
+from language_tool_python import LanguageTool
+def correct(text,language):
+    language_used= LanguageTool(language)
+    corrected_text = language_used.correct(text)
     print(f"Text before change: {text}")
-    print(f"Text after change: {new_text['result']}")
-    return new_text['result']
-correct("Hello my name are Mostfa Jenhani and I is a compter science engineerig student, I are 23 years old , I loves gaming and gambling ")
+    print(f"Text after change: {corrected_text}")
+    return corrected_text
+text_to_correct = "Je mapele Mostfa , je suis 23 ans et j ai un etudiant en genie inforrmatique"
+L="fr"
+correct(text_to_correct,L)
